5288897a9dcb6e91cc2547b0509345c5e31db5a6
adrianmancusp <adrian.mancuso@gmail.com>
2017-05-25 20:47:25 +1000

    completed first three tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc (tempf)
+	(tempf - 32) * 5 / 9
+end
+
+def ctof (tempc)
+	(tempc * 1.8 + 32).round(1)
+end
+
+puts ctof(37).to_f
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add (x, y)
+	x + y
+end
+
+def subtract (x, y)
+	x - y
+end
+
+def sum (x)
+	result = 0
+	x.each { |a| result += a }
+	sum = result
+end
+
+def multiply (*args)
+	result = 1
+	args.each { |a| result *= a }
+	multiply = result
+end
+
+def power (x, y)
+	x ** y
+end
+
+def factorial (x)
+	y = x -1
+	y.times do
+		x *= y
+		y -= 1
+	end
+	if x == 0
+		x = 1
+	end
+	factorial = x
+end
+
